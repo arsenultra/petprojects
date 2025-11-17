058e712d4b8f4545195cb5036b7582178537c7b4
arsenultra <arsken66@gmail.com>
2025-11-17 15:10:49 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*JFrame frame = new JFrame();
+        frame.setTitle("Field of Wonders");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1000,1000);
+        frame.setResizable(false);
+        frame.setVisible(true);
+         */
+
+        /*
+        ImageIcon image = new ImageIcon("logo.png");
+        frame.setIconImage(image.getImage());
+        */
+
+        MyFrame myFrame = new MyFrame();
+
+    }
+}
