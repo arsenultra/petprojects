282b596cd181f9c8d46514d323d93f87653690c2
arsenultra <arsken66@gmail.com>
2025-11-17 16:42:11 +0600

    test

--- untitled/src/MyFrame.java
+++ untitled/src/MyFrame.java
@@ -1,13 +1,22 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class MyFrame extends JFrame{
      
     MyFrame(){
+        ImageIcon logo = new ImageIcon("logo.png");
         ImageIcon image = new ImageIcon("wheel.png");
 
         JLabel label = new JLabel();
         label.setText("Wheel of Fortune");
         label.setIcon(image);
+        label.setHorizontalTextPosition(JLabel.CENTER);
+        label.setVerticalTextPosition(JLabel.TOP);
+        label.setFont(new Font("MV Boli", Font.PLAIN,50));
+        //label.setVerticalAlignment(JLabel.CENTER);
+        //label.setHorizontalAlignment(JLabel.CENTER);
+        label.setBounds(250,75,600,600);
+
 
         ImageIcon icon = new ImageIcon("logo.png");
         this.setIconImage(icon.getImage());
@@ -15,6 +24,7 @@ public class MyFrame extends JFrame{
         this.setTitle("Wheel of Fortune");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(1000,1000);
+        this.setLayout(null);
         this.setResizable(false);
         this.setVisible(true);
         this.add(label);
